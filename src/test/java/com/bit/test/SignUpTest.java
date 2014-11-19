6c6bc611f4dc53d0b7a49fb42f62dee0e1f8f8b6
Asad Zaman <asad.zaman@hbo.com>
2014-11-19 18:52:54 -0500

    selenium grid file

--- src/test/java/com/bit/test/SignUpTest.java
+++ src/test/java/com/bit/test/SignUpTest.java
@@ -3,6 +3,7 @@ package com.bit.test;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -11,10 +12,10 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class SignUpTest 
 {
 
-	public static void main(String[] args) throws MalformedURLException, Exception
+	@Test
+	public void login() throws MalformedURLException, Exception
     {
-        //FirefoxDriver driver = new FirefoxDriver();
-         
+   
         DesiredCapabilities capability = DesiredCapabilities.firefox();
         WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.16:4444/wd/hub"), capability);
         driver.get("http://www.facebook.com");
